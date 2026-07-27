a6429c6f330a28d2952b06917b20052bb06c2157
scaryhairytoes <scaryhairytoes@github.com>
2026-07-27 15:44:20 -0500

    Add S3 storage adapter for resume uploads

--- payload.config.ts
+++ payload.config.ts
@@ -682,4 +682,21 @@ export default buildConfig({
       ],
     },
   ],
+  plugins: [
+    s3Storage({
+      collections: {
+        resumes: true,
+      },
+      bucket: process.env.S3_BUCKET || '',
+      config: {
+        credentials: {
+          accessKeyId: process.env.S3_ACCESS_KEY_ID || '',
+          secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || '',
+        },
+        region: process.env.S3_REGION || 'auto',
+        endpoint: process.env.S3_ENDPOINT || '',
+        forcePathStyle: true,
+      },
+    }),
+  ],
 })
